62879a10d85461049c013dd73020298276247b8f
ViacheslavDobriy <dobrov.slava@yandex.ru>
2023-05-19 11:24:11 +0800

    Home Work was done

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('Task 1');
+
+let temperature = 10;
+console.log(`Температура в Цельсиях: ${temperature}`);
+
+temperature = (9/5)*temperature + 32;
+
+console.log(`Температура в Фаренгейтах: ${temperature}`);
+
+console.log('Task 2');
+
+const myName = 'Slava';
+const admin = myName;
+
+console.log(`Имя админа: ${admin}`);
